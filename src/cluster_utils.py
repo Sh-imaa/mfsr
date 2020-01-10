76c73c76d6b4f156aa8f52747e522c75afb4be3f
Shimaa <shimaa.baraka@gmail.com>
2020-01-10 00:37:04 -0500

    cluster pathes

---
+++ src/cluster_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from pathlib import Path
+import os
+
+def env_to_path(path):
+    """Transorms an environment variable mention in a conf file
+    into its actual value. E.g. $HOME/clouds -> /home/vsch/clouds
+    Args:
+        path (str): path potentially containing the env variable
+    """
+    if not isinstance(path, str):
+        return path
+
+    path_elements = path.split("/")
+    for i, d in enumerate(path_elements):
+        if "$" in d:
+            path_elements[i] = os.environ.get(d.replace("$", ""))
+    if any(d is None for d in path_elements):
+        return ""
+        
+    return "/".join(path_elements)
